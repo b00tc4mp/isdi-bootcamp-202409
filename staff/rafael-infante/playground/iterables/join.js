4c20fb9b0820ce867dc12cb3c83ef3ed3fafba57
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-09-28 19:26:31 +0200

    add join & unshift methods #134

---
+++ staff/rafael-infante/playground/iterables/join.js
@@ -0,0 +1,27 @@
+var join = function (iterable, separator) {
+  var newString = ''
+  if (separator === undefined) {
+    separator = ','
+  }
+  for (let i = 0; i < iterable.length; i++) {
+    i === 0 ? newString += iterable[i]
+      : newString += separator + iterable[i]
+  }
+  return newString
+}
+
+var elements = { 0: 'Fire', 1: 'Air', 2: 'Water', length: 3 }
+
+console.log(elements)
+
+console.log('CASE with no separator')
+
+var str = join(elements)
+console.log(str);
+//"Fire,Air,Water"
+
+console.log('CASE with separator "-"')
+
+var str1 = join(elements, '-')
+console.log(str1);
+//"Fire-Air-Water"
