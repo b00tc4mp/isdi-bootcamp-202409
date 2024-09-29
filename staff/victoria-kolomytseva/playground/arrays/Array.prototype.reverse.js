f24159fce9504e28ab9d171d7fb537932bcd718e
Victoria Kolomytseva <vkolomytseva87@gmail.com>
2024-09-29 18:01:52 +0200

    ejercicios

---
+++ staff/victoria-kolomytseva/playground/arrays/Array.prototype.reverse.js
@@ -0,0 +1,7 @@
+console.log('Test array.prototype.reverse')
+
+console.log('CASE reverse array ')
+
+const animals = ["panda", "elefante", "mono", "jirafa", "leon"]
+const reverse = animals.reverse()
+console.log animals // numbers and reversed are both in reversed order [5, 1, 4, 2, 3]
---
+++ staff/victoria-kolomytseva/playground/arrays/array.prototype.indexOf.js
@@ -0,0 +1,25 @@
+console.log('Test Array.prototype.indexOf')
+
+console.log('CASE find the index of the  element')
+
+const nums = [5, 3, 4, 12, 1, 8, 1]
+console.log(nums.indexOf(1));
+//Expected output: 4
+
+// Start from index2
+console.log(nums.indexOf(1, 2));
+//Expected output: 6
+
+console.log(nums.indexOf(14))
+// Expected output: -1
+
+//Ejemplo con palabras
+
+var ropa = ["vestido", "camisa", "falda", "jeans", "chaqueta", "vestido"]
+console.log(ropa.indexOf("vestido"))
+//Expected output: 0
+
+console.log(ropa.indexOf("vestido", 2))
+
+console.log(pinguIndex)
+// Expected output:5
