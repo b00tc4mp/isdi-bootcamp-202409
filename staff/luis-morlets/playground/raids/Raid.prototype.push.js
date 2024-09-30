aa3f7df2cbb0efc9fb5653338183e4f1e559f4f2
LuisMorlets <luismorlets07@gmail.com>
2024-09-30 16:50:13 +0200

    add raid concat and push test and implementation #152

---
+++ staff/luis-morlets/playground/raids/Raid.prototype.push.js
@@ -0,0 +1,32 @@
+console.log('TEST Raid.prototype.push')
+
+var Raid = function () {
+    this.length = 0
+}
+
+Raid.prototype.push = function () {
+    for (var i = 0; i < arguments.length; i++) {
+        this[this.length] = arguments[i]
+        this.length++
+    }
+
+    return this.length
+}
+
+
+console.log('CASE add banana to fruits')
+
+var fruits = new Raid
+fruits[0] = 'apple'
+fruits[1] = 'pineapple'
+fruits[2] = 'raspberry'
+fruits[3] = 'orange'
+fruits.length = 4
+
+
+var length = fruits.push('banana');
+
+console.log(fruits);
+// Raid: { 0: 'apple', 1: 'pineapple', 2: 'raspberry', 3: 'orange', 4: 'banana', length: 5 }
+
+console.log(length);
