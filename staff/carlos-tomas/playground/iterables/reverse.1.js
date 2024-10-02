e4101d19ad8d4e55e2531a7ea841f9ff1b414f4a
carlos tomas <ctcarlos25@gmail.com>
2024-10-02 13:32:59 +0200

    upgrade reverse and for Each #55

--- staff/carlos-tomas/playground/iterables/reverse.1.js
+++ staff/carlos-tomas/playground/iterables/reverse.1.js
@@ -6,7 +6,6 @@ var reverse = function (iterable) {
     }
 
     iterable = obj
-    obj1 = obj
     return iterable
 
 }
@@ -15,14 +14,15 @@ console.log("TEST  reverse ")
 console.log("CASE que toda el objeto cambie de sentido ")
 
 
-var obj1 = { 0: 'one', 1: 'two', 2: 'three', 3: "four", 4: "five", 5: "six", length: 6 };
+var obj1 = { 0: 'one', 1: 'two', 2: 'three', length: 3 };
 
 var obj = { length: 0 }
 
 var reversed = reverse(obj1);
 
 console.log(reversed)
-// Expected output: "reversed:" {0:"six", 1:"five", 2:"four", 3: "th"}
+// Expected output: "reversed:" Array ["three", "two", "one"]
 
 console.log(obj1 === reversed)
-//True
+
+
