493123402d35bd389c0eeb1d4152dce111809aa3
Jonas Veron <jonas.veron@hotmail.com>
2024-10-02 16:02:35 +0200

    add for each, map, reverse WIP #49

---
+++ staff/jonas-veron/playground/arrays/Array.prototype.map.js
@@ -0,0 +1,12 @@
+console.log('TEST Array.prototype.map')
+
+console.log('CASE pass a function to map')
+
+var array = [1, 4, 9, 16];
+
+// Pass a function to map
+
+var map1 = array.map((x) => x * 2);
+
+console.log(map);
+// [2, 8, 18, 32]
--- staff/jonas-veron/playground/iterables/reverse.js
+++ staff/jonas-veron/playground/iterables/reverse.js
@@ -1,15 +1,14 @@
 console.log('TEST Array.prototype.reverse')
 
 var reverse = function(iterable){
-    var result = {}
-    var newLength = iterable.length  - 1
-    for( var i = 0; i <= iterable.length - 1; i++){
-        result[i] = iterable[newLength - i]
-        result.length = iterable.length
+//un for con la condicion a la mitad
+    for(var i = 0; i < Math.floor((iterable.length) / 2); i++){ 
+        var element = iterable[i]//usamos una variable auxiliar para guardar el elemento
+        iterable[i] = iterable[iterable.length - 1 - i]//igualamos el primer elemento con el ultimo
+        iterable[iterable.length - 1 - i] = element //al ultimo le asignamos el elemento guardado
     }
-    return result
+    return iterable
 }
-
 
 
 var nums = {0: 100, 1: 200, 2: 300, length: 3}
@@ -19,7 +18,8 @@ console.log(nums);
 
 console.log('Revertir los elementos en nums')
 
-
 var reversed = reverse(nums);
 console.log(reversed);
 // {0: 300, 1: 200, 2: 100, length: 3}
+
+console.log(nums === reversed)
