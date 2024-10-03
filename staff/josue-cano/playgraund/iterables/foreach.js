b3d1dd959845ea2d7dba17474c21b80bb8e5e739
JCEXCELSIOR <josuecanod@gmail.com>
2024-10-03 17:56:22 +0200

    remove folder practicas, and dom7

---
+++ staff/josue-cano/playgraund/iterables/foreach.js
@@ -0,0 +1,31 @@
+var forEach = function (iterable, callback) {
+    for (var i = 0; i < iterable.length; i++) {
+        var element = iterable[i]
+
+        callback(element)
+    }
+}
+
+console.log('TEST Array.prototype.forEach')
+
+console.log('CASE print characters in iterable')
+
+var chars = { 0: 'a', 1: 'b', 2: 'c', length: 3 }
+
+// forEach(chars, function (element) { console.log(element) })
+
+
+
+
+
+
+
+
+var nums = { 0: 100, 1: 200, 2: 300, length: 3 }
+var result = 0
+
+forEach(nums, function (num) {
+    result += num
+})
+console.log(result)
+// 600
