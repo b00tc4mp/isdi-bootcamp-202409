74c5b4fd57884efee03401ad522ef2a40730f0f5
Manuel Barzi <manuelbarzi@gmail.com>
2024-10-03 11:55:29 +0200

    add splice test case and implementation update #160

--- staff/manuel-barzi/playground/arrays/Array.prototype.splice.js
+++ staff/manuel-barzi/playground/arrays/Array.prototype.splice.js
@@ -8,3 +8,12 @@ console.log(nums)
 // [100, 200, 300]
 console.log(extracted)
 // [400, 500, 600, 700]
+
+console.log('CASE extract elements from index 2 and delete 2')
+
+var nums = [100, 200, 300, 400, 500, 600, 700]
+var extracted = nums.splice(1, 2)
+console.log(nums)
+// [100, 400, 500, 600, 700]
+console.log(extracted)
+// [200, 300]
--- staff/manuel-barzi/playground/iterables/splice.js
+++ staff/manuel-barzi/playground/iterables/splice.js
@@ -1,48 +1,59 @@
-var splice = function (iterable, start) {
-    /*
-    iterable ->  { 0: 100, 1: 200, 2: 300, 3: 400, 4: 500, 5: 600, 6: 700, length: 7 }
-    start -> 3
+var splice = function (iterable, start, deleteCount) {
+    if (arguments.length === 2) {
+        var removed = { length: 0 }
 
-    removed -> { length: 0 }
+        for (var i = start; i < iterable.length; i++) {
+            var element = iterable[i]
 
-    removed -> { 0: 400, length: 0 }
-    removed -> { 0: 400, length: 1 }
-    iterable -> { 0: 100, 1: 200, 2: 300, 4: 500, 5: 600, 6: 700, length: 7 }
-    iterable -> { 0: 100, 1: 200, 2: 300, 4: 500, 5: 600, 6: 700, length: 6 }
+            delete iterable[i]
 
-    removed -> { 0: 400, 1: 500, length: 1 }
-    removed -> { 0: 400, 1: 500, length: 2 }
-    iterable ->  { 0: 100, 1: 200, 2: 300, 5: 600, 6: 700, length: 6 }
-    iterable ->  { 0: 100, 1: 200, 2: 300, 5: 600, 6: 700, length: 5 }
+            removed[removed.length] = element
+            removed.length++
+        }
 
-    removed -> { 0: 400, 1: 500, 2: 600, length: 2 }
-    removed -> { 0: 400, 1: 500, 2: 600, length: 3 }
-    iterable ->  { 0: 100, 1: 200, 2: 300, 6: 700, length: 5 }
-    iterable ->  { 0: 100, 1: 200, 2: 300, 6: 700, length: 4 }
+        iterable.length -= removed.length
 
-    removed -> { 0: 400, 1: 500, 2: 600, 3: 700, length: 3 }
-    removed -> { 0: 400, 1: 500, 2: 600, 3: 700, length: 4 }
-    iterable ->  { 0: 100, 1: 200, 2: 300, length: 4 }
-    iterable ->  { 0: 100, 1: 200, 2: 300, length: 3 }
+        return removed
+    } else if (arguments.length === 3) {
+        // iterable -> { 0: 100, 1: 200, 2: 300, 3: 400, 4: 500, 5: 600, 6: 700, length: 7 }
+        // start -> 1
+        // deleteCount -> 2
 
-    return removed
-    */
+        var removed = { length: 0 }
 
-    var removed = { length: 0 }
+        for (var i = start; i < start + deleteCount; i++) {
+            var element = iterable[i]
 
-    for (var i = start; i < iterable.length; i++) {
-        var element = iterable[i]
+            delete iterable[i]
 
-        delete iterable[i]
+            removed[removed.length] = element
+            removed.length++
+        }
 
-        removed[removed.length] = element
-        removed.length++
+        // iterable -> { 0: 100, 3: 400, 4: 500, 5: 600, 6: 700, length: 7 }
+
+        // iterable -> { 0: 100, 1: 400, 3: 400, 4: 500, 5: 600, 6: 700, length: 7 }
+        // iterable -> { 0: 100, 1: 400, 3: 400, 4: 500, 5: 600, 6: 700, length: 7 }
+        // iterable -> { 0: 100, 1: 400, 2: 500, 3: 400, 4: 500, 5: 600, 6: 700, length: 7 }
+        // iterable -> { 0: 100, 1: 400, 2: 500, 3: 600, 4: 500, 5: 600, 6: 700, length: 7 }
+        // iterable -> { 0: 100, 1: 400, 2: 500, 3: 600, 4: 700, 5: 600, 6: 700, length: 7 }
+
+        for (var i = start + deleteCount; i < iterable.length; i++) {
+            var element = iterable[i]
+
+            iterable[i - deleteCount] = element
+        }
+
+        // iterable -> { 0: 100, 1: 400, 2: 500, 3: 600, 4: 700, 6: 700, length: 7 }
+        // iterable -> { 0: 100, 1: 400, 2: 500, 3: 600, 4: 700, length: 7 }
+
+        for (var i = iterable.length - deleteCount; i < iterable.length; i++)
+            delete iterable[i]
+
+        iterable.length -= deleteCount
+
+        return removed
     }
-
-    //iterable.length = iterable.length - removed.length
-    iterable.length -= removed.length
-
-    return removed
 }
 
 console.log('TEST splice')
@@ -55,3 +66,12 @@ console.log(nums)
 // { 0: 100, 1: 200, 2: 300, length: 3 }
 console.log(extracted)
 // { 0: 400, 1: 500, 2: 600, 3: 700, length: 4 }
+
+console.log('CASE extract elements from index 2 and delete 2')
+
+var nums = { 0: 100, 1: 200, 2: 300, 3: 400, 4: 500, 5: 600, 6: 700, length: 7 }
+var extracted = splice(nums, 1, 2)
+console.log(nums)
+// { 0: 100, 1: 400, 2: 500, 3: 600, 4: 700, length: 5 }
+console.log(extracted)
+// { 0: 200, 1: 300, length: 2 }
