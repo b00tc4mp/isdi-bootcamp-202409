f1bf5c518d4aafb06e0ebe77eeba7c71fce2dd7b
Jonas Veron <jonas.veron@hotmail.com>
2024-10-06 11:36:12 +0200

    add copiWithin Method WIP #49

--- staff/jonas-veron/playground/arrays/Array.prototype.copyWithin.js
+++ staff/jonas-veron/playground/arrays/Array.prototype.copyWithin.js
@@ -2,25 +2,25 @@ console.log('TEST Array.prototype.copyWithin')
 
 
 console.log('CASE copy with one element')
-
-var array = [1, 2, 3, 4, 5]
-var result = array.copyWithin(-2);
+var nums = [100, 200, 300, 400, 500]
+var result = nums.copyWithin(-2);
 console.log(result)
-// [1, 2, 3, 1, 2]
+// [100, 200, 300, 100, 200]
+
+
+console.log('CASE copy with one element')
+var nums = [100, 200, 300, 400, 500]
+var result = nums.copyWithin(2);
+console.log(result)
+// [100, 200, 100, 200, 300]
 
 
 console.log('CASE Copy to index 0 the element at index 3')
-
-const array1 = ['a', 'b', 'c', 'd', 'e']
-
+var array1 = ['a', 'b', 'c', 'd', 'e']
 console.log(array1.copyWithin(0, 3, 4));
 // ["d", "b", "c", "d", "e"]
 
-
 console.log('CASE Copy to index 1 all elements from index 3 to the end')
-
-
-
-
+var array1 = ['a', 'b', 'c', 'd', 'e']
 console.log(array1.copyWithin(1, 3));
-// ["d", "d", "e", "d", "e"]
+// ["a", "d", "e", "d", "e"]
--- staff/jonas-veron/playground/iterables/copyWithin.js
+++ staff/jonas-veron/playground/iterables/copyWithin.js
@@ -3,34 +3,58 @@ console.log('TEST copyWithin method in object')
 //2on a partir de cual empieza a copiar elem si es negativo, se cuenta desde el final( por defecto es 0)
 //3er hasta donde se copia(no inclusive) si es negativo, se cuenta desde el final. por defecto es la longitud del array
 
-//{0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5}
+// iterable -> {0: 100, 1: 200, 2: 300, 3: 400, 4: 500, length: 5}
+// result -> {0: 100, 1: 200, 2: 300, 3: 400, 4: 500, length: 5}
+// result -> {0: 100, 1: 200, 2: 100, 3: 400, 4: 500, length: 5}
+// result -> {0: 100, 1: 200, 2: 100, 3: 200, 4: 500, length: 5}
+// result -> {0: 100, 1: 200, 2: 100, 3: 200, 4: 300, length: 5}
 
+// var copyWithin = function(iterable, target){
+//  var result = {length:0}
+//  var start = iterable.length - target
+//  //hacemos un for para copiar el objeto original
+//    for(var i = 0; i < iterable.length; i++){
+//     var element = iterable[i]
+//     result[result.length] = element
+//     result.length++
+//     }
+//    for(var i = 0; i < start; i++){
+//     var element = iterable[i]
+//     result[target+i] = element
+//    }
+//     return result
+// }
 
 var copyWithin = function(iterable, target){
-    var result = {length:0}
-    for(var i = 0; i < iterable.length; i++){
-        var element = iterable[i]
-        result[result.length] = element
-    }
+ var extracted = {length:0}
+ for(var i = 0; i <= target; i++){
+  extracted[extracted.length] = iterable[i]
+  extracted.length++
+ }
+ for(var i = target; i < iterable.length; i++){
+  iterable[i] = extracted[i]
+ }
+ return iterable
 }
-
-
-
 
 
 console.log('CASE copy with one element')
 
-var array = {0: 1, 1: 2, 2: 3, 3: 4, 4: 5, length: 5}
-copyWithin(array, -2)
-//{0: 1, 1: 2, 2: 3, 3: 1, 4: 2, length: 5}
+var nums = {0: 100, 1: 200, 2: 300, 3: 400, 4: 500, length: 5}
+var result = copyWithin(nums, 2)
+console.log(result)
+//{0: 100, 1: 200, 2: 100, 3: 200, 4: 300, length: 5}
 
+console.log('CASE copy with one element')
 
-
-
+var nums = {0: 100, 1: 200, 2: 300, 3: 400, 4: 500, length: 5}
+var result = copyWithin(nums, -2)
+console.log(result)
+//{0: 100, 1: 200, 2: 300, 3: 100, 4: 200, length: 5}
 
 console.log('CASE Copy to index 0 the element at index 3')
 
-const array1 = ['a', 'b', 'c', 'd', 'e']
+var array1 = ['a', 'b', 'c', 'd', 'e']
 
 console.log(array1.copyWithin(0, 3, 4));
 // ["d", "b", "c", "d", "e"]
