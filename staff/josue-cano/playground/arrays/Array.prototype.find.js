9ab4c3ddeca2aca31b1cae5e5efa37f6bb3bd373
JCEXCELSIOR <josuecanod@gmail.com>
2024-10-07 08:34:37 +0200

    no finally find

---
+++ staff/josue-cano/playground/arrays/Array.prototype.find.js
@@ -0,0 +1,7 @@
+console.log('TEST Array.prototype.find')
+
+
+const numeros = [1, 3, 7, 9, 2];
+
+const encontrado = numeros.find(numero => numero > 5);
+console.log(encontrado)
---
+++ staff/josue-cano/playground/iterables/find.js
@@ -0,0 +1,18 @@
+
+
+var find = function(elemento,index, ){
+
+}
+
+
+var frutas= {
+    0: manzana,
+    1: pera,
+    2: naranja,
+    3:platano,
+    length : 4
+}
+
+
+
+
