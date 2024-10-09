6c6168a783d91689ac1ab520c9a288073d860f2c
annagonzalez9 <sol.anna.9@gmail.com>
2024-10-09 09:44:22 +0200

    add password restriction in login submit #167

--- staff/anna-gonzalez/unsocial/logic.js
+++ staff/anna-gonzalez/unsocial/logic.js
@@ -2,6 +2,9 @@ function loginUser(username, password) {
     if (username.length < 4 || username.length > 12)
         throw new Error('Invalid username')
 
+    if (password.length < 8)
+        throw new Error('Invalid password')
+
     var user = users.find(function (user) {
         return user.username === username && user.password === password
     })
