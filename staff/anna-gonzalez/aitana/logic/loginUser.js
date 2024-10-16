9c86ba77227e04102d28fee6edddfcdba8220f3f
annagonzalez9 <sol.anna.9@gmail.com>
2024-10-16 18:15:53 +0200

    add aitana page #167

---
+++ staff/anna-gonzalez/aitana/logic/loginUser.js
@@ -0,0 +1,14 @@
+const loginUser = (username, password) => {
+    if (username.length < 4 || username.length > 12)
+        throw new Error('Invalid username')
+
+    if (password.length < 8)
+        throw new Error('Invalid password')
+
+    const user = users.find(user => user.username === username && user.password === password)
+
+    if (user === undefined)
+        throw new Error('User not registered')
+
+    return user
+}
