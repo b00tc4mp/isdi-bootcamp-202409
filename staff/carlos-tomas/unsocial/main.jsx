b482df7e6256bc2695336b8f52ee2a30e6c08708
carlos tomas <ctcarlos25@gmail.com>
2024-10-19 11:49:57 +0200

    How home, create post and data local storage works #175

--- staff/carlos-tomas/unsocial/main.jsx
+++ staff/carlos-tomas/unsocial/main.jsx
@@ -1,34 +1,36 @@
-let loggedInUser = null
-
-const rootElment = document.getElementById("root")
-const root = ReactDOM.createRoot(rootElment)
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement)
 
 const { Component } = React
 
 class PasswordInput extends Component {
     constructor(props) {
-        super(props)
+        console.log('PasswordInput -> constructor')
 
-        this.state = { status: '😌', type: "password" }
+        super(props) // this.props = props
+
+        this.state = { status: '😌', type: 'password' }
     }
 
     render() {
+        console.log('PasswordInput -> render')
 
-        return <div style={{ display: "flex" }}>
-            <input type={this.state.type} id={this.props.id} style={{ width: "100%", bosSizing: "boder-box", paddingRight: "18px" }} />
+        return <div style={{ display: 'flex' }}>
+            <input type={this.state.type} id={this.props.id} style={{ width: '100%', boxSizing: 'border-box', paddingRight: '18px' }} />
             <span
-                style={{ cursor: "pointer", position: "absolute", right: "10px" }}
+                style={{ cursor: 'pointer', position: 'absolute', right: '10px' }}
                 onClick={() => this.setState({
-                    status: this.state.status === '😌' ? "😳" : "😌",
-                    type: this.state.type === "password" ? "text" : "password"
+                    status: this.state.status === '😌' ? '😳' : '😌',
+                    type: this.state.type === 'password' ? 'text' : 'password'
                 })}
             >{this.state.status}</span>
-        </div >
-
+        </div>
     }
 }
 
 function Login(props) {
+    console.log('Login -> render')
+
     return <section>
         <h2>Login</h2>
 
@@ -38,22 +40,21 @@ function Login(props) {
             const { target: { username: { value: username }, password: { value: password } } } = event
 
             try {
-                loggedInUser = authenticateUser(username, password)
+                sessionStorage.loggedInUserId = authenticateUser(username, password)
 
                 event.target.reset()
 
                 props.onLoggedIn()
             } catch (error) {
+                //passwordInput.setValue('')
 
                 alert(error.message)
 
                 console.error(error)
             }
-
-
         }}>
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" style={{ width: "100%", boxSizing: "border-box" }} />
+            <input type="text" id="username" style={{ width: '100%', boxSizing: 'border-box' }} />
 
             <label htmlFor="password">Password</label>
             <PasswordInput id="password" />
@@ -61,32 +62,39 @@ function Login(props) {
             <button type="submit">Login</button>
         </form>
 
-        <a href=" "
-            onClick={event => {
-                event.preventDefault()
+        <a href="" onClick={event => {
+            event.preventDefault()
 
-                props.RegiForm()
-
-            }}>Register</a>
+            props.onRegisterClick()
+        }}>Register</a>
     </section>
 }
 
 function Register(props) {
+    console.log('Register -> render')
+
     return <section>
         <h2>Register</h2>
+
         <form onSubmit={event => {
             event.preventDefault()
 
-            const { target: { name: { value: name }, username: { value: username }, email: { value: email }, password: { value: password }, passwordRepeat: { value: passwordRepeat } } } = event
-            try {
+            const { target: form } = event
 
+            const {
+                name: { value: name },
+                email: { value: email },
+                username: { value: username },
+                password: { value: password },
+                ['password-repeat']: { value: passwordRepeat }
+            } = form
+
+            try {
                 registerUser(name, email, username, password, passwordRepeat)
 
-                event.target.reset()
+                form.reset()
 
-                props.returnLogin()
-
-
+                props.onRegistered()
             } catch (error) {
                 alert(error.message)
 
@@ -94,83 +102,156 @@ function Register(props) {
             }
         }}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" style={{ width: "100%", boxSizing: "border-box" }} />
+            <input type="text" id="name" style={{ width: '100%', boxSizing: 'border-box' }} />
 
             <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" style={{ width: "100%", boxSizing: "border-box" }} />
+            <input type="email" id="email" style={{ width: '100%', boxSizing: 'border-box' }} />
 
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" style={{ width: "100%", boxSizing: "border-box" }} />
+            <input type="text" id="username" style={{ width: '100%', boxSizing: 'border-box' }} />
 
             <label htmlFor="password">Password</label>
             <PasswordInput id="password" />
 
-            <label htmlFor="passwordRepeat">Repeat Password</label>
-            <PasswordInput id="passwordRepeat" />
+            <label htmlFor="password-repeat">Repeat Password</label>
+            <PasswordInput id="password-repeat" />
 
             <button type="submit">Register</button>
         </form>
-        <a href=""
-            onClick={event => {
-                event.preventDefault()
 
-                props.returnLogin()
+        <a href="" onClick={event => {
+            event.preventDefault()
 
-            }}>Login</a>
-    </section >
-}
-
-function Home(props) {
-    return <section>
-        <h2>Home</h2>
-
-        <h3>Hello, Peter Pan!</h3>
-        <button type="button"
-            onClick={event => {
-                event.preventDefault()
-
-                props.returnLogin()
-
-            }}>Logout</button>
-        <button type="button">➕</button>
-
-        <div>
-            <h3>Posts</h3>
-
-            <article>
-                <h4>wendydarling</h4>
-                <img src="https://pm1.aminoapps.com/8360/ad07e2d2cdf6e1733328d6e7b7848b87db38a2bbr1-1536-2048v2_hq.jpg" style={{ width: "100%" }} />
-                <p>here i am</p>
-                <time>Thu Oct 17 2024 15:21:27 GMT+0200 (hora de verano de Europa central)</time>
-            </article>
-
-            <article>
-                <h4>peterpan</h4>
-                <img src="https://i.pinimg.com/originals/8c/60/1a/8c601a25311a1a5098896f751a784b54.jpg" style={{ width: "100%" }} />
-                <p>here we are</p>
-                <time>Thu Oct 17 2024 15:21:27 GMT+0200 (hora de verano de Europa central)</time>
-            </article>
-        </div>
+            props.onLoginClick()
+        }}>Login</a>
     </section>
 }
 
-
-
-class App extends Component {
+class Home extends Component {
     constructor(props) {
+        console.log('Home -> constructor')
+
         super(props)
 
-        this.state = { view: "login" }
+        let name
+
+        try {
+            name = getUserName(sessionStorage.loggedInUserId)
+        } catch (error) {
+            alert(error.message)
+
+            console.error(error)
+        }
+
+        this.state = { name: name, view: 'list' }
     }
 
     render() {
+        console.log('Home -> render')
+
+        return <section>
+            <h2>Home</h2>
+
+            <h3>Hello, {this.state.name}!</h3>
+            <button type="button" onClick={() => {
+                delete sessionStorage.loggedInUserId
+
+                this.props.onLoggedOut()
+            }}>Logout</button>
+            <button type="button" onClick={() => this.setState({ view: 'new' })}>➕</button>
+
+            {this.state.view === 'list' && <PostList />}
+            {this.state.view === 'new' && <CreatePost onCreated={() => this.setState({ view: 'list' })} />}
+        </section>
+    }
+}
+
+function PostList() {
+    console.log('PostList -> render')
+
+    let posts
+
+    try {
+        posts = getPosts()
+    } catch (error) {
+        alert(error.message)
+
+        console.error(error)
+    }
+
+    return <div>
+        <h3>Posts</h3>
+
+        {posts.map(post => <article>
+            <h4>{post.author}</h4>
+            <img src={post.image} style={{ width: '100%' }} />
+            <p>{post.text}</p>
+            <time>{post.date}</time>
+        </article>)}
+    </div>
+}
+
+function CreatePost(props) {
+    console.log('CreatePost -> render')
+
+    return <div>
+        <h3>Create Post</h3>
+
+        <form onSubmit={event => {
+            event.preventDefault()
+
+            const { target: form } = event
+
+            const {
+                image: { value: image },
+                text: { value: text }
+            } = form
+
+            try {
+                createPost(sessionStorage.loggedInUserId, image, text)
+
+                props.onCreated()
+            } catch (error) {
+                alert(error.message)
+
+                console.error(error)
+            }
+        }}>
+            <label htmlFor="image">Image</label>
+            <input type="text" id="image" style={{ width: '100%', boxSizing: 'border-box' }} />
+
+            <label htmlFor="text">Text</label>
+            <input type="text" id="text" style={{ width: '100%', boxSizing: 'border-box' }} />
+
+            <button type="submit">Create</button>
+        </form>
+    </div>
+}
+
+class App extends Component {
+    constructor(props) {
+        console.log('App -> constructor')
+
+        super(props)
+
+        this.state = { view: 'login' }
+    }
+
+    render() {
+        console.log('App -> render')
+
         return <div>
             <h1>Unsocial</h1>
 
-            {this.state.view === "login" && <Login onLoggedIn={() => this.setState({ view: "home" })} RegiForm={() => this.setState({ view: "register" })} />}
-            {this.state.view === "register" && <Register returnLogin={() => this.setState({ view: "login" })} />}
-            {this.state.view === "home" && <Home returnLogin={() => this.setState({ view: "login" })} />}
-
+            {this.state.view === 'login' && <Login
+                onLoggedIn={() => this.setState({ view: 'home' })}
+                onRegisterClick={() => this.setState({ view: 'register' })}
+            />}
+            {this.state.view === 'register' && <Register
+                onLoginClick={() => this.setState({ view: 'login' })}
+                onRegistered={() => this.setState({ view: 'login' })}
+            />}
+            {this.state.view === 'home' && <Home onLoggedOut={() => this.setState({ view: 'login' })} />}
         </div>
     }
 }
