322e3b5c546d4ce38f830db21bfbbe820d2897bf
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-10-19 09:43:46 +0200

    add uuid function #181

--- staff/rafael-infante/unsocial/data/users.js
+++ staff/rafael-infante/unsocial/data/users.js
@@ -1,4 +1,23 @@
 const users = [
-  { name: 'Peter Pan', email: 'peter@pan.com', username: 'peterpan', password: '123123123' },
-  { name: 'Wendy Darling', email: 'wendy@darling.com', username: 'wendydarling', password: '123123123' }
+  {
+    userId: uuid(),
+    name: 'Peter Pan',
+    email: 'peter@pan.com',
+    username: 'peterpan',
+    password: '123123123'
+  },
+  {
+    userId: uuid(),
+    name: 'Wendy Darling',
+    email: 'wendy@darling.com',
+    username: 'wendydarling',
+    password: '123123123'
+  },
+  {
+    userId: uuid(),
+    name: 'Captain Hook',
+    email: 'captain@hook.com',
+    username: 'captainhook',
+    password: '123123123'
+  }
 ]
