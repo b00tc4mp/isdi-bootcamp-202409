72994d0b54c86bfa2b2b16417ab7ece7d333af4d
CriBeriso <berith93@icloud.com>
2024-10-21 18:31:59 +0200

    add old version of app (pre- vite) #171

--- staff/cris-beriso/unsocial.1/data/posts.js
+++ staff/cris-beriso/unsocial.1/data/posts.js
@@ -1,14 +1,16 @@
 const posts = [
   {
+    id: 'm2eund561og',
     image: '../unsocial/Cofy.jpg',
     text: 'Cofy',
-    username: 'beriso',
+    author: 'm2euqjhm6ak',
     date: new Date
   },
   {
+    id: 'm2euoh62cv8',
     image: '../unsocial/Rocket.jpg',
     text: 'Rocket',
-    username: 'beriso',
+    author: 'm2euqjhm6ak',
     date: new Date
   }
 ]
--- staff/cris-beriso/unsocial.1/data/users.js
+++ staff/cris-beriso/unsocial.1/data/users.js
@@ -1,5 +1,5 @@
 const users = [
-  { name: 'Cris Beriso', email: 'cris@beriso.com', username: 'beriso', password: 'criscris' },
-  { name: 'Peter Pan', email: 'peter@pan.com', username: 'peterpan', password: '123123123' },
-  { name: 'Wendy Darling', email: 'wendy@darling.com', username: 'wendydarling', password: '123123123' }
+  { id: 'm2euqjhm6ak', name: 'Cris Beriso', email: 'cris@beriso.com', username: 'beriso', password: 'criscris' },
+  { id: 'm2eo2tqvg06', name: 'Peter Pan', email: 'peter@pan.com', username: 'peterpan', password: '123123123' },
+  { id: 'm2eo38l0e1', name: 'Wendy Darling', email: 'wendy@darling.com', username: 'wendydarling', password: '123123123' }
 ]
