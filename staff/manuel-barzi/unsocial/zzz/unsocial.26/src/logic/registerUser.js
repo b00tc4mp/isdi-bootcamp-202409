68f0418fb683f78fa0b9d338b678b3482c28590f
Manuel Barzi <manuelbarzi@gmail.com>
2024-10-28 13:51:53 +0100

    restructure folders for app and api #165

---
+++ staff/manuel-barzi/unsocial/zzz/unsocial.26/src/logic/registerUser.js
@@ -0,0 +1,24 @@
+import { validate } from './helpers'
+
+import uuid from '../data/uuid'
+
+export default (name, email, username, password, passwordRepeat) => {
+    validate.name(name)
+    validate.email(email)
+    validate.username(username)
+    validate.password(password)
+    validate.passwordsMatch(password, passwordRepeat)
+
+    const users = JSON.parse(localStorage.users)
+
+    let user = users.find(user => user.username === username || user.email === email)
+
+    if (user !== undefined)
+        throw new Error('user already exists')
+
+    user = { id: uuid(), name: name, email: email, username: username, password: password }
+
+    users.push(user)
+
+    localStorage.users = JSON.stringify(users)
+}
