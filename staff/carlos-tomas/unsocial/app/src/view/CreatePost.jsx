812422bad75f1b9dcd29fbb40fbeddc04a74255b
carlos tomas <ctcarlos25@gmail.com>
2024-10-28 15:36:59 +0100

    Modify the unsocial folder to app and create the apis #175

--- staff/carlos-tomas/unsocial/app/src/view/CreatePost.jsx
+++ staff/carlos-tomas/unsocial/app/src/view/CreatePost.jsx
@@ -8,29 +8,29 @@ import logic from '../logic'
 export default ({ onCreated }) => {
     console.log('CreatePost -> render')
 
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        const { target: form } = event
+
+        const {
+            image: { value: image },
+            text: { value: text }
+        } = form
+
+        try {
+            logic.createPost(image, text)
+
+            onCreated()
+        } catch (error) {
+            alert(error.message)
+
+            console.error(error)
+        }
+    }
+
     return <main className='CreatePost'>
-        <h3>Create Post</h3>
-
-        <Form onSubmit={event => {
-            event.preventDefault()
-
-            const { target: form } = event
-
-            const {
-                image: { value: image },
-                text: { value: text }
-            } = form
-
-            try {
-                logic.createPost(image, text)
-
-                onCreated()
-            } catch (error) {
-                alert(error.message)
-
-                console.error(error)
-            }
-        }}>
+        <Form onSubmit={handleSubmit}>
             <Field>
                 <Label htmlFor="image">Image</Label>
                 <Input type="text" id="image" />
--- staff/carlos-tomas/unsocial/app/src/view/Login.jsx
+++ staff/carlos-tomas/unsocial/app/src/view/Login.jsx
@@ -8,26 +8,28 @@ import logic from '../logic'
 export default (props) => {
     console.log('Login -> render')
 
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        const { target: { username: { value: username }, password: { value: password } } } = event
+
+        try {
+            logic.loginUser(username, password)
+
+            event.target.reset()
+
+            props.onLoggedIn()
+        } catch (error) {
+            alert(error.message)
+
+            console.error(error)
+        }
+    }
+
     return <main className="Login">
         <h2>Login</h2>
 
-        <Form onSubmit={event => {
-            event.preventDefault()
-
-            const { target: { username: { value: username }, password: { value: password } } } = event
-
-            try {
-                logic.loginUser(username, password)
-
-                event.target.reset()
-
-                props.onLoggedIn()
-            } catch (error) {
-                alert(error.message)
-
-                console.error(error)
-            }
-        }}>
+        <Form onSubmit={handleSubmit}>
             <Field>
                 <Label htmlFor="username">Username</Label>
                 <Input type="text" id="username" />
