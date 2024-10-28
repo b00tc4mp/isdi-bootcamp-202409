1a2a9d6556a8ac6a2f41985255a702044ad50cff
CriBeriso <berith93@icloud.com>
2024-10-28 15:55:27 +0100

    separate app, api and zzz folders #171

---
+++ staff/cris-beriso/unsocial/zzz/unsocial.1/data/users.js
@@ -0,0 +1,5 @@
+const users = [
+  { id: 'm2euqjhm6ak', name: 'Cris Beriso', email: 'cris@beriso.com', username: 'beriso', password: 'criscris' },
+  { id: 'm2eo2tqvg06', name: 'Peter Pan', email: 'peter@pan.com', username: 'peterpan', password: '123123123' },
+  { id: 'm2eo38l0e1', name: 'Wendy Darling', email: 'wendy@darling.com', username: 'wendydarling', password: '123123123' }
+]
---
+++ staff/cris-beriso/unsocial/zzz/unsocial.1/logic/registerUser.js
@@ -0,0 +1,41 @@
+/**
+ * Recogemos los datos del nuevo usuario y 
+ * los guardamos en USER (despues de unas comprobaciones).
+ * Antes de guardar los datos de user en users, tenemos que convertir users en objeto,
+ * para ello usamos JSON.parse. Corroboramos que no hay coincidencias y almacenamos la info.
+ * Pusheamos user a users
+ * @param {string} name 
+ * @param {string tipo email} email 
+ * @param {string} username 
+ * @param {string} password 
+ * @param {string} passwordRepeat 
+ */
+const registerUser = (name, email, username, password, passwordRepeat) => {
+  if (name.length < 2)
+    throw new Error('invalid name')
+
+  if (!/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email))
+    throw new Error('invalid e-mail')
+
+  if (username.length < 4 || username.length > 12)
+    throw new Error('invalid username')
+
+  if (password.length < 8)
+    throw new Error('invalid password')
+
+  if (password !== passwordRepeat)
+    throw new Error('passwords do not match')
+
+  const users = JSON.parse(localStorage.users)
+
+  let user = users.find(user => user.username === username || user.email === email)
+
+  if (user !== undefined)
+    throw new Error('user already exists')
+
+  user = { id: uuid(), name: name, email: email, username: username, password: password }
+
+  users.push(user)
+
+  localStorage.users = JSON.stringify(users)
+}
