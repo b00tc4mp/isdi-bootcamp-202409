0a13f522359a2a813994ef6ae67cace3cbca02a0
Javier Romera Claros <Javi@MacBook-Air-de-Montserrat.local>
2024-10-28 18:03:14 +0100

    add chat #46

---
+++ staff/javier-romera/playground/chat/client.js
@@ -0,0 +1,28 @@
+const net = require('net')
+const readline = require('readline')
+
+const client = net.createConnection(6666, "192.168.1.109", () => {
+    console.log('connected to server')
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+
+    rl.question('who are you?', name => {
+        client.write(JSON.stringify({ type: 'id', name }))
+
+        function chat() {
+            rl.question('write to?', name => {
+                rl.question('what message?', message => {
+                    client.write(JSON.stringify({ type: 'text', to: name, message }))
+
+                    chat()
+                })
+            })
+        }
+
+        chat()
+    })
+    client.on('data', data => console.log(data.toString()))
+})
