8c85a91138253120f255a9a6fb1f32c23c3e8b69
Blu <pablodelgado@Ps-MB.local>
2024-10-29 18:22:08 +0100

    add app-server + web-server #125

---
+++ staff/pablo-delgado/playground/app-server/index.js
@@ -0,0 +1,56 @@
+import express from 'express'
+
+// data
+
+let uuid = 0
+const users = []
+
+// presentation + business (logic)
+
+const server = express()
+
+const formBodyParser = express.urlencoded({ extended: true })
+
+server.use(express.static('public'))
+
+server.post('/login', formBodyParser, (req, res) => {
+    const { username, password } = req.body
+
+    const user = users.find(user => user.username === username && user.password === password)
+
+    if (!user) {
+        res.send('wrong credentials babesss')
+
+        return
+    }
+    res.send(`welcome ${user.name}`)
+})
+
+server.post('/register', formBodyParser, (req, res) => {
+    const { name, email, username, password, 'password-repeat': passwordRepeat } = req.body
+
+    let user = users.find(user => user.email === email || user.username === username)
+
+    if (user) {
+        res.send('user already exists u dummie')
+
+        return
+    }
+
+    if (password !== passwordRepeat) {
+        res.send('password do not match, try again')
+
+        return
+    }
+
+    user = { id: ++uuid, name, email, username, password }
+
+    users.push(user)
+    console.log(users)
+
+    res.send('user registered')
+})
+
+server.listen(8080, () => console.log('server is up'))
+
+//TODO use cookies for session mgmt (RTFM cookies + express)
