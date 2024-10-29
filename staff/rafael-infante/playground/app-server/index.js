9affdb387d28f089c6a3d15a38d1397c3d0393c0
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-10-29 23:21:55 +0100

    build app-server using express js #134

---
+++ staff/rafael-infante/playground/app-server/index.js
@@ -0,0 +1,54 @@
+import express from 'express'
+
+const users = []
+let uuid = 0
+
+const server = express()
+
+const formBodyParser = express.urlencoded({ extended: true })
+
+server.use(express.static('public'))
+
+// http://localhost:8080/login
+//// { username: 'peterpan', password: '123123123' }
+
+server.post('/login', formBodyParser, (req, res) => {
+  const { username, password } = req.body
+
+  let user = users.find(user => user.username === username && user.password === password)
+
+  if (!user) {
+    res.send('wrong credentials')
+
+    return
+  }
+
+  res.send(`welcome ${user.name}`)
+})
+
+server.post('/register', formBodyParser, (req, res) => {
+  const { name, email, username, password, 'password-repeat': passwordRepeat } = req.body
+
+  let user = users.find(user => user.email === email || user.username === username)
+
+  if (user) {
+    res.send('user already exists')
+
+    return
+  }
+
+  if (password !== passwordRepeat) {
+    res.send('passwords do not match')
+
+    return
+  }
+
+  user = { id: ++uuid, name, email, username, password }
+
+  users.push(user)
+  console.log(users)
+
+  res.send('user registered')
+})
+
+server.listen(8080, () => console.log('server is up'))
