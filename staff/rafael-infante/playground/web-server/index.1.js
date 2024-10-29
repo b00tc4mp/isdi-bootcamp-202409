5fe5c7d3b87753a54df4b87f06e5364a2f906eea
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-10-29 19:59:01 +0100

    create a web server with http requests #134

---
+++ staff/rafael-infante/playground/web-server/index.1.js
@@ -0,0 +1,18 @@
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain')
+
+  res.write(`<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hola, Web!</title>
+    </head>
+    <body>
+        <h1>Hola, Web!</h1>
+    </body>
+</html>`)
+  res.end()
+})
+
+server.listen(8080, () => console.log('server is up'))
