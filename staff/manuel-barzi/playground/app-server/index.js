258db7610fb09a2332860626e23f7374c2766cef
Manuel Barzi <manuelbarzi@gmail.com>
2024-10-30 10:37:48 +0100

    add session control with cookies in server; separate concerns (logic, data, util); #160

--- staff/manuel-barzi/playground/app-server/index.js
+++ staff/manuel-barzi/playground/app-server/index.js
@@ -1,11 +1,5 @@
 import express from 'express'
-
-// data
-
-let uuid = 0
-const users = []
-
-// presentation + business (logic)
+import logic from './logic/index.js'
 
 const server = express()
 
@@ -13,43 +7,94 @@ const formBodyParser = express.urlencoded({ extended: true })
 
 server.use(express.static('public'))
 
-server.post('/login', formBodyParser, (req, res) => { // http://localhost:8080/login [username=pepito&password=123123123]
-    const { username, password } = req.body
+server.get('/login', (req, res) => {
+    const userId = req.headers.cookie?.split('=')[1]
 
-    const user = users.find(user => user.username === username && user.password === password)
-
-    if (!user) {
-        res.send('wrong credentials')
+    if (userId) {
+        res.redirect('/')
 
         return
     }
 
-    res.send(`welcome ${user.name}`)
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <h1>Login</h1>
+
+    <form action="/login" method="post">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+
+        <button type="submit">Login</button>
+    </form>
+</body>
+
+</html>`)
 })
 
-server.post('/register', formBodyParser, (req, res) => { // http://localhost:8080/register [name=Peter+Pan&email=peter%40pan.com&username=peterpan&password=123123123&password-repeat=123123123]
+server.get('/', (req, res) => {
+    const userId = req.headers.cookie?.split('=')[1]
+
+    if (!userId) {
+        res.redirect('/login')
+
+        return
+    }
+
+    res.send(`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+
+<body>
+    <h1>Home</h1>
+</body>
+
+</html>`)
+})
+
+server.post('/login', formBodyParser, (req, res) => {
+    const { username, password } = req.body
+
+    try {
+        const userId = logic.authenticateUser(username, password)
+
+        res.setHeader('set-cookie', `userId=${userId}`)
+
+        res.redirect('/')
+    } catch (error) {
+        res.send(error.message)
+
+        console.error(error)
+    }
+})
+
+server.post('/register', formBodyParser, (req, res) => {
     const { name, email, username, password, 'password-repeat': passwordRepeat } = req.body
 
-    let user = users.find(user => user.email === email || user.username === username)
+    try {
+        logic.registerUser(name, email, username, password, passwordRepeat)
 
-    if (user) {
-        res.send('user already exists')
+        res.send('user registered')
+    } catch (error) {
+        res.send(error.message)
 
-        return
+        console.error(error)
     }
-
-    if (password !== passwordRepeat) {
-        res.send('passwords do not match')
-
-        return
-    }
-
-    user = { id: ++uuid, name, email, username, password }
-
-    users.push(user)
-    console.log(users)
-
-    res.send('user registered')
 })
 
 server.listen(8080, () => console.log('server is up'))
