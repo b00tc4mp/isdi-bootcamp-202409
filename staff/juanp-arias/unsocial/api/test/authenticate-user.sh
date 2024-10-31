d246aa8045dd3e671fe4fff0f79b8e53cacc087f
Juan Pablo Arias <juanpabloariasr1@icloud.com>
2024-10-31 14:47:27 +0100

    add api to unsocial with logic #177

---
+++ staff/juanp-arias/unsocial/api/test/authenticate-user.sh
@@ -0,0 +1,1 @@
+ curl -H 'Content-Type: application/json' -d '{"username":"pepitogrillo","password":"123456"}' http://localhost:7070/authenticate -v
---
+++ staff/juanp-arias/unsocial/api/test/get-user-name.sh
@@ -0,0 +1,1 @@
+ curl http://localhost:7070/users/m2x63gb7wns/name -v
