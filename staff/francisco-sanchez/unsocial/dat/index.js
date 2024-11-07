e1143cc3fee418a067b678431cbeacd1a39c16d0
nomadwebs <programacion@nomadwebs.com>
2024-11-07 17:50:16 +0100

    create date dat folder #172

---
+++ staff/francisco-sanchez/unsocial/dat/index.js
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient('mongodb://127.0.0.1:27017')
+
+client.connect()
+    .then(connection => {
+        console.log('Database connected')
+
+        const db = connection.db('unsocial')
+
+        const users = db.collection('users')
+        const posts = db.collection('posts')
+
+        users.find({}).toArray().then(users => console.log(users))
+        users.find({}).toArray().then(posts => console.log(JSON.stringify(posts, null, 2)))
+
+
+        users.insertOne({ name: 'pepe', username: 'pepet2', email: 'pepet2@marieta.com', password: '1234' })
+        users.find({}).toArray().then(users => console.log(users))
+
+    })
