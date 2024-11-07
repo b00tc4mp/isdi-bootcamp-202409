2682a1a3aae0ba2c07f8f71cf1752cde322498b0
LuisMorlets <luismorlets07@gmail.com>
2024-11-07 18:09:45 +0100

    created dat package for managing mongodb; add script to connect to mongodb with node drivers #184

--- staff/luis-morlets/unsocial/app/src/components/library/PasswordInput.jsx
+++ staff/luis-morlets/unsocial/app/src/components/library/PasswordInput.jsx
@@ -3,12 +3,12 @@ import Input from './Input'
 
 export default function PasswordInput({ id }) {
     const [type, setType] = useState('password')
-    const [status, setStatus] = useState('https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/show-password-3.png')
+    const [status, setStatus] = useState('https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/hide-password.png')
 
     const handleToggleClick = () => {
-        setStatus(status === 'https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/show-password-3.png'
-            ? 'https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/hide-password.png'
-            : 'https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/show-password-3.png')
+        setStatus(status === 'https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/hide-password.png'
+            ? 'https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/show-password-3.png'
+            : 'https://icons.veryicon.com/png/o/miscellaneous/computer-room-integration/hide-password.png')
         setType(type === 'password'
             ? 'text'
             : 'password')
