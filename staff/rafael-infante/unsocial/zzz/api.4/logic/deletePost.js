566975506f9cfd69a35c7be4e951b27ed4869fef
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-11-07 18:48:35 +0100

    install mongodb packages, set the connection to the mongodb server and make data queries for practice #181

---
+++ staff/rafael-infante/unsocial/zzz/api.4/logic/deletePost.js
@@ -0,0 +1,24 @@
+import { validate } from "./helpers/index.js"
+import { storage } from "../data/index.js"
+
+export default (userId, postId) => {
+
+  validate.id(userId, 'userId')
+  validate.id(postId, 'postId')
+
+  const { posts } = storage
+
+  const index = posts.findIndex(post => post.id === postId)
+
+  if (index < 0) throw new Error('post not found')
+
+  const post = posts[index]
+
+  const { author } = post
+
+  if (author !== userId) throw new Error('user is not author of post')
+
+  posts.splice(index, 1)
+
+  storage.posts = posts
+}
