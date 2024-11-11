445b26456714fbe3bf003da8c7db42d296f4d05e
Manuel Barzi <manuelbarzi@gmail.com>
2024-11-11 10:57:44 +0100

    implement create-post and delete-post server-side logics #165

--- staff/manuel-barzi/unsocial/api/logic/deletePost.test.js
+++ staff/manuel-barzi/unsocial/api/logic/deletePost.test.js
@@ -1,7 +1,15 @@
+import db from 'dat'
 import deletePost from './deletePost.js'
 
-try {
-    deletePost('m2vvqdtgcba', 'm2x2g9dhsdo')
-} catch (error) {
-    console.error(error)
-}
+db.connect('mongodb://localhost/unsocial-test')
+    .then(() => {
+        try {
+            return deletePost('672e025c405e99414c72a716', '6731d0771cde96a6b6ca0f65')
+                .then(console.log) // undefined
+                .catch(console.error)
+        } catch (error) {
+            console.error(error)
+        }
+    })
+    .catch(console.error)
+    .finally(() => db.disconnect())
--- staff/manuel-barzi/unsocial/api/logic/registerUser.test.js
+++ staff/manuel-barzi/unsocial/api/logic/registerUser.test.js
@@ -5,15 +5,12 @@ import registerUser from './registerUser.js'
 db.connect('mongodb://127.0.0.1:27017/unsocial-test')
     .then(() => {
         try {
-            registerUser('Coco Drilo', 'coco@drilo.com', 'cocodrilo', '123123123', '123123123')
-                .then(() => console.log('user registered'))
+            return registerUser('Coco Drilo', 'coco@drilo.com', 'cocodrilo', '123123123', '123123123')
+                .then(console.log) // undefined
                 .catch(console.error)
-
-            // registerUser('Coco Drilo', 'coco@drilo.com', 'cocodrilo', '123123123', '123123123')
-            //     .then(() => console.log('user registered'))
-            //     .catch(console.error)
         } catch (error) {
             console.error(error)
         }
     })
     .catch(console.error)
+    .finally(() => db.disconnect())
