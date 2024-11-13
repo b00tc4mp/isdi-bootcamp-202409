f400920147cfd5ca3b669864282d5b75892a3d69
Juan Pablo Arias <juanpabloariasr1@icloud.com>
2024-11-13 18:12:26 +0100

    migrate to mongoose #177

--- staff/juanp-arias/unsocial/api/logic/authenticateUser.js
+++ staff/juanp-arias/unsocial/api/logic/authenticateUser.js
@@ -1,13 +1,14 @@
-import db from 'dat'
+import { models } from 'dat'
 import { validate, errors } from 'com'
 
+const { User } = models
 const { SystemError, CredentialsError } = errors
 
 export default (username, password) => {
     validate.username(username)
     validate.password(password)
 
-    return db.users.findOne({ username, password })
+    return User.findOne({ username, password })
         .catch(error => { throw new SystemError(error.message) })
         .then(user => {
             if (!user) throw new CredentialsError('wrong credentials')
--- staff/juanp-arias/unsocial/dat/index.js
+++ staff/juanp-arias/unsocial/dat/index.js
@@ -1,16 +1,17 @@
-import { MongoClient, ObjectId } from 'mongodb'
+import mongoose from 'mongoose'
+import models from './models.js'
+
+const { ObjectId } = mongoose.Types
 
 function connect(mongoUrl) {
-    const client = new MongoClient(mongoUrl)
-
-    return client.connect()
-        .then(connection => {
-            const db = connection.db()
+    return mongoose.connect(mongoUrl)
+        .then(() => {
+            const db = mongoose.connection.db
 
             this.users = db.collection('users')
             this.posts = db.collection('posts')
 
-            this.disconnect = () => connection.close()
+            this.disconnect = () => mongoose.disconnect()
         })
 }
 
@@ -22,3 +23,7 @@ const db = {
     ObjectId
 }
 export default db
+
+export {
+    models
+}
