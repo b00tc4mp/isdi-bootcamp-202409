9681e8ae8f0768b9613d6f1ff356e6167ce89410
Manuel Barzi <manuelbarzi@gmail.com>
2024-11-13 12:26:50 +0100

    refactor api, add error-handler, authorization-handler and functional-handler factory; refactor errors in com, add custom-error factory; #165

--- staff/manuel-barzi/unsocial/api/index.js
+++ staff/manuel-barzi/unsocial/api/index.js
@@ -1,194 +1,82 @@
 import db from 'dat'
 import express, { json } from 'express'
-import logic from './logic/index.js'
 import cors from 'cors'
-import { errors } from 'com'
 
-const { ValidationError, SystemError, DuplicityError, CredentialsError, NotFoundError, OwnershipError } = errors
+import logic from './logic/index.js'
+import { createFunctionalHandler, authorizationHandler, errorHandler } from './helpers/index.js'
 
-db.connect('mongodb://127.0.0.1:27017/unsocial-test')
-    .then(() => {
-        console.log('connected to db')
+db.connect('mongodb://127.0.0.1:27017/unsocial-test').then(() => {
+    console.log('connected to db')
 
-        const server = express()
+    const server = express()
 
-        server.use(cors())
+    server.use(cors())
 
-        const jsonBodyParser = json()
+    const jsonBodyParser = json()
 
-        server.get('/', (_, res) => res.send('Hello, API!'))
+    server.get('/', (_, res) => res.send('Hello, API!'))
 
-        server.post('/users/auth', jsonBodyParser, (req, res, next) => {
-            try {
-                const { username, password } = req.body
+    server.post('/users/auth', jsonBodyParser, createFunctionalHandler((req, res) => {
+        const { username, password } = req.body
 
-                logic.authenticateUser(username, password)
-                    .then(userId => res.json(userId))
-                    //.catch(error => next(error))
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+        return logic.authenticateUser(username, password).then(userId => res.json(userId))
+    }))
 
-        server.post('/users', jsonBodyParser, (req, res, next) => {
-            try {
-                const { name, email, username, password, 'password-repeat': passwordRepeat } = req.body
+    server.post('/users', jsonBodyParser, createFunctionalHandler((req, res) => {
+        const { name, email, username, password, 'password-repeat': passwordRepeat } = req.body
 
-                logic.registerUser(name, email, username, password, passwordRepeat)
-                    .then(() => res.status(201).send())
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+        return logic.registerUser(name, email, username, password, passwordRepeat).then(() => res.status(201).send())
+    }))
 
-        server.get('/users/:targetUserId/name', (req, res, next) => {
-            try {
-                const userId = req.headers.authorization.slice(6)
+    server.get('/users/:targetUserId/name', authorizationHandler, createFunctionalHandler((req, res) => {
+        const { userId, params: { targetUserId } } = req
 
-                const { targetUserId } = req.params
+        return logic.getUserName(userId, targetUserId).then(name => res.json(name))
+    }))
 
-                logic.getUserName(userId, targetUserId)
-                    .then(name => res.json(name))
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+    server.post('/posts', jsonBodyParser, authorizationHandler, createFunctionalHandler((req, res) => {
+        const { userId, body: { image, text } } = req
 
-        server.post('/posts', jsonBodyParser, (req, res, next) => {
-            try {
-                const userId = req.headers.authorization.slice(6)
+        return logic.createPost(userId, image, text).then(() => res.status(201).send())
+    }))
 
-                const { image, text } = req.body
+    server.get('/posts', authorizationHandler, createFunctionalHandler((req, res) => {
+        const { userId } = req
 
-                logic.createPost(userId, image, text)
-                    .then(() => res.status(201).send())
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+        return logic.getPosts(userId).then(posts => res.json(posts))
+    }))
 
-        server.get('/posts', (req, res, next) => {
-            try {
-                const userId = req.headers.authorization.slice(6)
+    server.delete('/posts/:postId', authorizationHandler, createFunctionalHandler((req, res) => {
+        const { userId, params: { postId } } = req
 
-                logic.getPosts(userId)
-                    .then(posts => res.json(posts))
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+        return logic.deletePost(userId, postId).then(() => res.status(204).send())
+    }))
 
-        server.delete('/posts/:postId', (req, res, next) => {
-            try {
-                const userId = req.headers.authorization.slice(6)
+    server.patch('/posts/:postId/likes', authorizationHandler, createFunctionalHandler((req, res) => {
+        const { userId, params: { postId } } = req
 
-                const { postId } = req.params
+        return logic.toggleLikePost(userId, postId).then(() => res.status(204).send())
+    }))
 
-                logic.deletePost(userId, postId)
-                    .then(() => res.status(204).send())
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+    server.post('/posts/:postId/comments', authorizationHandler, jsonBodyParser, createFunctionalHandler((req, res) => {
+        const { userId, params: { postId }, body: { text } } = req
 
-        server.patch('/posts/:postId/likes', (req, res, next) => {
-            try {
-                const userId = req.headers.authorization.slice(6)
+        return logic.addComment(userId, postId, text).then(() => res.status(201).send())
+    }))
 
-                const { postId } = req.params
+    server.delete('/posts/:postId/comments/:commentId', authorizationHandler, createFunctionalHandler((req, res) => {
+        const { userId, params: { postId, commentId } } = req
 
-                logic.toggleLikePost(userId, postId)
-                    .then(() => res.status(204).send())
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+        return logic.removeComment(userId, postId, commentId).then(() => res.status(204).send())
+    }))
 
-        server.post('/posts/:postId/comments', jsonBodyParser, (req, res, next) => {
-            try {
-                const userId = req.headers.authorization.slice(6)
+    server.get('/posts/:postId/comments', authorizationHandler, createFunctionalHandler((req, res) => {
+        const { userId, params: { postId } } = req
 
-                const { params: { postId }, body: { text } } = req
+        return logic.getComments(userId, postId).then(comments => res.json(comments))
+    }))
 
-                logic.addComment(userId, postId, text)
-                    .then(() => res.status(201).send())
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
+    server.use(errorHandler)
 
-        server.delete('/posts/:postId/comments/:commentId', (req, res, next) => {
-            const userId = req.headers.authorization.slice(6)
-
-            const { postId, commentId } = req.params
-
-            try {
-                logic.removeComment(userId, postId, commentId)
-                    .then(() => res.status(204).send())
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
-
-        server.get('/posts/:postId/comments', (req, res, next) => {
-            try {
-                const userId = req.headers.authorization.slice(6)
-
-                const { postId } = req.params
-
-                logic.getComments(userId, postId)
-                    .then(comments => res.json(comments))
-                    .catch(next)
-            } catch (error) {
-                next(error)
-            }
-        })
-
-        server.use((error, req, res, next) => {
-            let status = 500
-
-            // if (error instanceof ValidationError)
-            //     status = 406
-            // else if (error instanceof NotFoundError)
-            //     status = 404
-            // else if (error instanceof CredentialsError)
-            //     status = 401
-            // else if (error instanceof DuplicityError)
-            //     status = 409
-            // else if (error instanceof OwnershipError)
-            //     status = 403
-
-            switch (true) {
-                case (error instanceof ValidationError):
-                    status = 406
-                    break
-                case (error instanceof NotFoundError):
-                    status = 404
-                    break
-                case (error instanceof CredentialsError):
-                    status = 401
-                    break
-                case (error instanceof DuplicityError):
-                    status = 409
-                    break
-                case (error instanceof OwnershipError):
-                    status = 403
-                    break
-            }
-
-            res.status(status).json({ error: status === 500 ? SystemError.name : error.constructor.name, message: error.message })
-
-            console.error(error)
-        })
-
-        server.listen(8080, () => console.log('api is up'))
-    })
+    server.listen(8080, () => console.log('api is up'))
+})
