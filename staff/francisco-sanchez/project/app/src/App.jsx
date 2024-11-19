aca62488a72bc7b884625a6be2e5a966a078900e
nomadwebs <programacion@nomadwebs.com>
2024-11-20 00:30:51 +0100

    Create folders for project and install Vite + React + TailwindCss, but tailwindcss is not working properly #219

---
+++ staff/francisco-sanchez/project/app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <body class="bg-red-300">
+
+      <Header />
+    </body>
+  )
+}
+
+export default App
---
+++ staff/francisco-sanchez/project/app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="h-full">
+            <p className="h-full text-6xl">Header</p>
+        </div>
+    )
+}
+
+export default Header
