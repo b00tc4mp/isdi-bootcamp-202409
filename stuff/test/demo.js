83d73ffa0e003cdecd2ab0b3a3bdff2f121c2913
Manuel Barzi <manuelbarzi@gmail.com>
2024-11-20 12:07:37 +0100

    add chai-as-promised like to test demo #163

--- stuff/test/demo.js
+++ stuff/test/demo.js
@@ -8,9 +8,12 @@ function it(description, callback) {
     console.log('TEST', description)
 
     try {
-        callback()
+        const value = callback()
+
+        if (value instanceof Promise)
+            value.catch(error => console.error('FAIL', error.message))
     } catch (error) {
-        console.error('KO', error.message)
+        console.error('FAIL', error.message)
     }
 }
 
@@ -19,10 +22,57 @@ function expect(value) {
         get to() {
             return {
                 equal(expectedValue) {
-                    if (value === expectedValue)
-                        console.log('OK')
-                    else
-                        throw new Error(`expect ${expectedValue} is to equal ${value}`)
+                    if (value !== expectedValue)
+                        throw new Error(`expect ${value} is to equal ${expectedValue}`)
+                },
+
+                get have() {
+                    return {
+                        lengthOf(expectedValue) {
+                            if (value.length !== expectedValue)
+                                throw new Error(`expect ${value} to have length of ${expectedValue}`)
+                        }
+                    }
+                },
+
+                throw(errorType, message) {
+                    let expectedError
+
+                    try {
+                        value()
+                    } catch (error) {
+                        expectedError = error
+                    } finally {
+                        if (!(expectedError instanceof errorType)) throw new Error(`expected ${value} to throw ${errorType}`)
+                        if (typeof message === 'string' && !expectedError.message.includes(message) || message instanceof RegExp && !message.test(expectedError.message)) throw new Error(`expected ${value} to throw error with message ${message}`)
+                    }
+                },
+
+                get be() {
+                    return {
+                        rejectedWith(errorType, message) {
+                            let expectedError
+
+                            return value
+                                .catch(error => expectedError = error)
+                                .finally(() => {
+                                    if (!(expectedError instanceof errorType)) throw new Error(`expected ${value} to throw ${errorType}`)
+                                    if (typeof message === 'string' && !expectedError.message.includes(message) || message instanceof RegExp && !message.test(expectedError.message)) throw new Error(`expected ${value} to throw error with message ${message}`)
+                                })
+                        }
+                    }
+                },
+
+                get eventually() {
+                    return {
+                        equal(expectedValue) {
+                            return value
+                                .then(value1 => {
+                                    if (value1 !== expectedValue)
+                                        throw new Error(`expected ${value} to eventually resolve with value equal to ${expectedValue}`)
+                                })
+                        }
+                    }
                 }
             }
         }
@@ -31,16 +81,51 @@ function expect(value) {
 
 // demo
 
+function authenticate(username, password) {
+    if (username === 'pepito' && password === '123123123')
+        return 'abc123'
+
+    throw new Error('wrong credentials')
+}
+
+function authenticateAsync(username, password) {
+    return new Promise((resolve, reject) => {
+        try {
+            resolve(authenticate(username, password))
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
 describe('getPosts', () => {
     console.log('...')
 
-    describe('when user whatever', () => {
+    describe('...', () => {
         console.log('...')
     })
 
-    it('succeeds on same number', () => {
+    it('...', () => {
         console.log('...')
 
-        expect(1).to.equal(2)
+        //expect(1).to.equal(2)
+        //expect([1, 2, 3]).to.have.lengthOf(3)
+
+        //expect(() => { throw new TypeError('hello error 1') }).to.throw(TypeError, /^hello error$/)
+        // expect(() => { throw new TypeError('hello error 1') }).to.throw(TypeError, 'hello error')
+        //return expect(Promise.reject(new TypeError('hello async error'))).to.be.rejectedWith(TypeError, /^hello async error$/)
+
+        //expect(authenticate('pepito', '123123123')).to.equal('abc123')
+        //expect(() => authenticate('pepito', '123123123_')).to.throw(Error, 'wrong credentials')
+
+        //return expect(authenticateAsync('pepito', '123123123')).to.eventually.equal('abc123')
+        return expect(authenticateAsync('pepito', '123123123_')).to.be.rejectedWith(Error, /^wrong credentials$/)
     })
 })
+
+
+
+
+
+
+
