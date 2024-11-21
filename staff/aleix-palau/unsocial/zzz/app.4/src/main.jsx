d6a447e11307f90372018e659af0703f17d1f8b7
AL Eix <al331x@gmail.com>
2024-11-21 09:22:24 +0100

    install react-router and hello/search functionalities #169

--- staff/aleix-palau/unsocial/zzz/app.4/src/main.jsx
+++ staff/aleix-palau/unsocial/zzz/app.4/src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <React.StrictMode>
-    <Router>
+    <React.StrictMode>
         <App />
-    </Router>
-    // </React.StrictMode>
+    </React.StrictMode>,
 )
