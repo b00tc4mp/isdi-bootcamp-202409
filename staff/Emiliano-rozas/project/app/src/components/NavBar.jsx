aab9330214c0f8d483db473396c157b2f48bf5b1
Emiliano Rozas <emilianorozas@gmail.com>
2024-11-21 11:13:56 +0100

    add Navbar with routes and styles #232

--- staff/Emiliano-rozas/project/app/src/components/NavBar.jsx
+++ staff/Emiliano-rozas/project/app/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets } from '../assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const NavBar = () => {
     return (
@@ -8,7 +8,7 @@ const NavBar = () => {
 
             <img src={assets.logoBuenardo} className='w-36' alt="" />
 
-            <ul className='hidden sm:flex gap-5 text-sm text-green-700'>
+            <ul className='hidden sm:flex gap-5 text-sm text-white'>
                 <NavLink to='/' className={'flex flex-col items-center gap-1'}>
                     <p>HOME</p>
                     <hr className='w-2/4 border-none h-[1.5px] bg-green-700 hidden' />
@@ -26,10 +26,29 @@ const NavBar = () => {
                     <hr className='w-2/4 border-none h-[1.5px] bg-green-700 hidden' />
                 </NavLink>
             </ul>
-            <div className='flex items-center gap-6'></div>
-            <img src="" alt="" />
+            <div className='flex items-center gap-6'>
+                <img src={assets.searchIcon} className='w-5 cursor-pointer' alt="" />
 
+                <div className='group relative'>
+                    <img src={assets.profileIcon} className='w-5 cursor-pointer' alt="" />
+                    <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 '>
+                        <div className='flex flex-col gap-2 w-36 py-3 px-5 border-solid border-2  border-green-700 text-white '>
+                            <p className='cursor-pointer hover:text-green-700'>My Profile</p>
+                            <NavLink to='/orders'>
+                                <p className='cursor-pointer hover:text-green-700'>Orders</p>
+                            </NavLink>
+                            <p className='cursor-pointer hover:text-green-700'>Logout</p>
+                        </div>
+                    </div>
+                </div>
+                <Link to='/cart' className='relative'>
+                    <img src={assets.cartIcon} className='w-5 cursor-pointer' alt="" />
+                </Link>
+                <img src={assets.menuIcon} className='w-5 cursor-pointer' alt="" />
+            </div>
         </div>
+
+
     )
 }
 
