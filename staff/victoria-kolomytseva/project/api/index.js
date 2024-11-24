5c456102c644e661def6331c880ae8eb2df61f08
Victoria Kolomytseva <vkolomytseva87@gmail.com>
2024-11-24 15:53:07 +0100

    install tailwind #258

---
+++ staff/victoria-kolomytseva/project/api/index.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import db from 'dat'
+import express, { json } from 'express'
+import cors from 'cors'
+import jwt from 'jsonwebtoken'
+
+
+
+db.connect(process.env.MONGO_URL).then(() => {
+    console.log('connected to db')
+
+    const server = express()
+
+    server.use(cors())
+
+    const jsonBodyParser = json()
+
+    server.get('/', (_, res) => res.send('Hello, API!'))
+
+
+
+    server.listen(process.env.PORT, () => console.log(`API listening on port ${process.env.PORT}`))
+})
