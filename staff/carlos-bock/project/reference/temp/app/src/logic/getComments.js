1da7ef0ce46a3d7be1cc25863edaf2b32382ef46
CRBM84 <carlosrbock@gmail.com>
2024-11-25 11:02:44 +0100

    update documentation #230

---
+++ staff/carlos-bock/project/reference/temp/app/src/logic/getComments.js
@@ -0,0 +1,35 @@
+import { validate, errors } from 'com';
+
+const { SystemError } = errors;
+
+const getComments = (postId, callback) => {
+    validate.id(postId, 'postId');
+    validate.callback(callback);
+
+    const xhr = new XMLHttpRequest;
+
+    xhr.addEventListener('load', () => {
+        const {status, response} = xhr;
+
+        if (status === 200) {
+            const comments = JSON.parse(response);
+
+            callback(null, comments);
+
+            return;
+        }
+
+        const { error, message } = JSON.parse(response);
+
+        const constructor = errors[error];
+
+        callback(new constructor(message));
+    });
+    xhr.addEventListener('error', () => callback(new SystemError('server error')));
+
+    xhr.open('GET', `http://${import.meta.env.VITE_API_URL}/posts/${postId}/comments`)
+    xhr.setRequestHeader('Authorization', `Bearer ${sessionStorage.token}`);
+    xhr.send();
+}
+
+export default getComments;
---
+++ staff/carlos-bock/project/reference/temp/app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.htlm',
+    './src/**/*.jsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
