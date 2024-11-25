264ef28ab674f300355a574e771d5069374b410e
Javier Romera Claros <Javi@MacBook-Air-de-Montserrat.local>
2024-11-25 09:43:37 +0100

    implement fetch in getPosts app logic #168

--- staff/javier-romera/unsocial/app/src/logic/getPosts.js
+++ staff/javier-romera/unsocial/app/src/logic/getPosts.js
@@ -3,22 +3,22 @@ import { errors } from 'apu'
 const { SystemError } = errors
 
 export default () => {
-    const options = {
+    return fetch(`http://${import.meta.env.VITE_API_URL}/posts`, {
         headers: {
             'Authorization': `Bearer ${localStorage.token}`
         }
-    }
-
-    return fetch(`http://${import.meta.env.VITE_API_URL}/posts`, options)
+    })
         .catch(error => { throw new SystemError(error.message) })
         .then(res => {
             const { status } = res;
 
             if (status === 200) {
                 return res.json()
+                    .catch(error => { throw new SystemError(error.message) })
             }
 
             return res.json()
+                .catch(error => { throw new SystemError(error.message) })
                 .then(res => {
                     const { error, message } = res
 
