b70ed0e99a2d10a36619d205512f4b8e6aa83478
JCEXCELSIOR <josuecanod@gmail.com>
2024-11-26 16:40:40 +0100

    add fetch, async and localstorage! #182

--- staff/josue-cano/unsocial/api/logic/authenticateUser.js
+++ staff/josue-cano/unsocial/api/logic/authenticateUser.js
@@ -8,19 +8,30 @@ export default (username, password) => {
     validate.username(username)
     validate.password(password)
 
-    return User.findOne({ username })
-        .catch(error => { throw new SystemError(error.message) })
-        .then(user => {
-            if (!user) throw new CredentialsError('wrong credentials')
+    return (async () => {
+        let user
 
-            return bcrypt.compare(password, user.password)
-                .then(match => {
-                    if (!match) throw new CredentialsError('wrong credentials')
+        try {
+            user = await User.findOne({ username })
+        } catch (error) {
+            throw new SystemError(error.message)
+        }
 
-                    return {
-                        id: user._id.toString(),
-                        role: user.role
-                    }
-                })
-        })
+        if (!user) throw new CredentialsError('wrong credentials')
+
+        let match
+
+        try {
+            match = await bcrypt.compare(password, user.password)
+        } catch (error) {
+            throw new SystemError(error.message)
+        }
+
+        if (!match) throw new CredentialsError('wrong credentials')
+
+        return {
+            id: user._id.toString(),
+            role: user.role
+        }
+    })()
 }
--- staff/josue-cano/unsocial/api/logic/getUserName.js
+++ staff/josue-cano/unsocial/api/logic/getUserName.js
@@ -1,19 +1,29 @@
-import { User } from 'dat'
-import { validate, errors } from 'com'
+import { User } from "dat";
+import { validate, errors } from "com";
 
-
-const { SystemError, NotFoundError } = errors
+const { SystemError, NotFoundError } = errors;
 
 export default (userId, targetUserId) => {
-    validate.id(userId, 'userId')
-    validate.id(targetUserId, 'targetUserId')
+  validate.id(userId, "userId");
+  validate.id(targetUserId, "targetUserId");
 
-    return Promise.all([User.findById(userId).lean(), User.findById(targetUserId).lean()])
-        .catch(error => { throw new SystemError(error.message) })
-        .then(([user, targetUser]) => {
-            if (!user) throw new NotFoundError('user not found')
-            if (!targetUser) throw new NotFoundError('target user not found')
+  return (async () => {
+    let users;
 
-            return targetUser.name
-        })
-}
+    try {
+      users = await Promise.all([
+        User.findById(userId).lean(),
+        User.findById(targetUserId).lean(),
+      ]);
+    } catch (error) {
+      throw new SystemError(error.message);
+    }
+
+    const [user, targetUser] = users;
+
+    if (!user) throw new NotFoundError("user not found");
+    if (!targetUser) throw new NotFoundError("target user not found");
+
+    return targetUser.name;
+  })();
+};
