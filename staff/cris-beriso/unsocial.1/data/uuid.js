0098cfeb86b9b4d61679a020cfbda67719dc64e2
CriBeriso <berith93@icloud.com>
2024-11-27 15:32:55 +0100

    delete unsocial old version #171

--- staff/cris-beriso/unsocial.1/data/uuid.js
+++
@@ -1,7 +0,0 @@
-/**
- * Suma los milisegundos y un numero random, lo convierte en string y 
- * le quita el punto de los decimales
- * @returns Devuelte un id de tipo string 
- * Lo usaremos para generar ids unicos a usuarios y publicaciones
- */
-const uuid = () => (Date.now() + Math.random()).toString(36).replace('.', '')
