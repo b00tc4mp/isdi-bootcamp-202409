2e17e946fc901958cefdd249b05be147adc8c5e9
Manuel Barzi <manuelbarzi@gmail.com>
2024-11-29 10:56:15 +0100

    re-organise folders in api; add routers to group handlers;  #165

--- staff/manuel-barzi/unsocial/api/logic/index.js
+++ staff/manuel-barzi/unsocial/api/logic/index.js
@@ -1,22 +1,28 @@
-import authenticateUser from './authenticateUser.js'
-import registerUser from './registerUser.js'
-import getUserName from './getUserName.js'
-import createPost from './createPost.js'
-import getPosts from './getPosts.js'
-import deletePost from './deletePost.js'
-import toggleLikePost from './toggleLikePost.js'
-import addComment from './addComment.js'
-import removeComment from './removeComment.js'
-import getComments from './getComments.js'
+import {
+    registerUser,
+    authenticateUser,
+    getUserName
+} from './users/index.js'
+
+import {
+    createPost,
+    deletePost,
+    getPosts,
+    toggleLikePost,
+
+    addComment,
+    removeComment,
+    getComments
+} from './posts/index.js'
 
 const logic = {
-    authenticateUser,
     registerUser,
+    authenticateUser,
     getUserName,
 
     createPost,
-    getPosts,
     deletePost,
+    getPosts,
     toggleLikePost,
 
     addComment,
