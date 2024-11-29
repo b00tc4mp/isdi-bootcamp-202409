b6be56238e945bef56f2ec2f1241fb0109e9ba94
Manuel Barzi <manuelbarzi@gmail.com>
2024-11-29 11:56:59 +0100

    group logic in folders for users and posts in app #165

--- staff/manuel-barzi/unsocial/app/src/logic/index.js
+++ staff/manuel-barzi/unsocial/app/src/logic/index.js
@@ -1,41 +1,43 @@
-import registerUser from './registerUser'
-import loginUser from './loginUser'
-import isUserLoggedIn from './isUserLoggedIn'
-import getUserName from './getUserName'
-import logoutUser from './logoutUser'
-import getUserId from './getUserId'
-import getUserRole from './getUserRole'
-import isUserRoleRegular from './isUserRoleRegular'
-import isUserRoleModerator from './isUserRoleModerator'
+import {
+    registerUser,
+    loginUser,
+    getUserName,
+    isUserLoggedIn,
+    isUserRoleModerator,
+    isUserRoleRegular,
+    getUserId,
+    logoutUser
+} from './users'
 
-import createPost from './createPost'
-import getPosts from './getPosts'
-import toggleLikePost from './toggleLikePost'
-import deletePost from './deletePost'
+import {
+    createPost,
+    deletePost,
+    getPosts,
+    toggleLikePost,
 
-import addComment from './addComment'
-import getComments from './getComments'
-import removeComment from './removeComment'
+    addComment,
+    removeComment,
+    getComments
+} from './posts'
 
 const logic = {
     registerUser,
     loginUser,
-    isUserLoggedIn,
     getUserName,
-    logoutUser,
-    getUserId,
-    getUserRole,
-    isUserRoleRegular,
+    isUserLoggedIn,
     isUserRoleModerator,
+    isUserRoleRegular,
+    getUserId,
+    logoutUser,
 
     createPost,
+    deletePost,
     getPosts,
     toggleLikePost,
-    deletePost,
 
     addComment,
-    getComments,
-    removeComment
+    removeComment,
+    getComments
 }
 
 export default logic
