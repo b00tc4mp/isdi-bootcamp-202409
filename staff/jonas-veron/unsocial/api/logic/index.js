167296e75ac1395028749aa28534491a788ce363
Jonas Veron <jonas.veron@hotmail.com>
2024-11-29 12:49:50 +0100

    reorganized folders in api and implement express-router #166

--- staff/jonas-veron/unsocial/api/logic/index.js
+++ staff/jonas-veron/unsocial/api/logic/index.js
@@ -1,16 +1,14 @@
-import authenticateUser from "./authenticateUser.js";
-import registerUser from "./registerUser.js";
-import getUserName from "./getUserName.js";
+import { registerUser, authenticateUser, getUserName } from "./users/index.js"
 
-import getPosts from "./getPosts.js";
-import createPost from "./createPost.js";
-import deletePost from "./deletePost.js";
-
-import toggleLikePost from "./toggleLikePost.js";
-
-import addComment from "./addComment.js";
-import removeComment from "./removeComment.js";
-import getComments from "./getComments.js";
+import {
+  createPost,
+  deletePost,
+  getPosts,
+  toggleLikePost,
+  addComment,
+  removeComment,
+  getComments,
+} from "./posts/index.js"
 
 const logic = {
   authenticateUser,
@@ -26,6 +24,6 @@ const logic = {
   addComment,
   removeComment,
   getComments,
-};
+}
 
-export default logic;
+export default logic
