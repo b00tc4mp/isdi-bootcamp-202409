2889103f74038bca1191d2653a6a279f4b37806d
salvatore marchese <smarchese985@yahoo.co.uk>
2024-12-05 18:09:04 +0100

    file untracked #261

---
+++ staff/salvatore-marchese/dive-and-discover/api/logic/homeCenter.js
@@ -0,0 +1,22 @@
+import { errors } from "com";
+
+
+
+const { ValidationError } = errors
+
+export default async function createHomeCenter({ name, email, password, address, postcode, city, country, telephone, timetable }) {
+    if (!name || !email || !password || !address || !postcode || !city || !country || !telephone || !timetable ) {
+        throw new ValidationError('All fields are required.')
+    }
+
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+        throw new ValidationError('User with this email already exist.')
+    }
+
+    const user = new user({ name, email, password }) 
+
+    await user.save()
+
+    return { message: 'Home Center created successfully!' }
+}
