86749ec074cb87f1177114e8b92fa45aa25d886f
salvatore marchese <smarchese985@yahoo.co.uk>
2024-12-05 18:11:01 +0100

    api index, logic; app logic, view #261

---
+++ staff/salvatore-marchese/dive-and-discover/app/src/logic/getUserName.js
@@ -0,0 +1,26 @@
+import { errors } from '../../../com'
+import { extractPayloadFromJWT } from '../util'
+
+const { SystemError } = errors 
+
+export default async () => { 
+    let { sub: userId } = 
+    extractPayloadFromJWT(sessionStorage.token)
+
+    return fetch(`http://${import.meta.env.VITE_API_URL}/users/${userId}/name`, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.token}`
+        }
+    })
+        .catch(error => { throw new SystemError(error.message) })
+        .then (res => {
+            if (res.ok)
+                return res.json()
+                    .catch(error => {
+                    throw new SystemError(error.message) })
+                    .then(body => body.name)
+            return res.json()
+                .catch(error => { throw new SystemError(error.message) })
+                .then(({ error, message}) => { throw new errors[error](message) })
+        })
+}
