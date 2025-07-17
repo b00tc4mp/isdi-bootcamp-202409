77449db8f40392a379bb3d93e54565cc00aa9b91
pablodpp3 <pablodporttillo@gmail.com>
2025-07-17 17:49:20 +0200

    added user logics and routes, models, errors, validators & updated utils folder #220

---
+++ staff/pablo-delgado/project2/app/src/logic/index.js
@@ -0,0 +1,23 @@
+import {
+    registerUser,
+    loginUser,
+    getUserName,
+    isUserLoggedIn,
+    isUserRoleModerator,
+    isUserRoleRegular,
+    getUserId,
+    logoutUser
+} from './users'
+
+const logic = {
+    registerUser,
+    loginUser,
+    getUserName,
+    isUserLoggedIn,
+    isUserRoleModerator,
+    isUserRoleRegular,
+    getUserId,
+    logoutUser
+}
+
+export default logic
